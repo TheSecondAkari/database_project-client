a8e18ab7a8e12b3e2b8a3be5139679ff135076d5
ChenXinyu <378330403@qq.com>
2019-12-16 10:18:57 +0800

    fixed

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -21,6 +20,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10px;
 }
 </style>
